3fd3a4e7b884e66e6c47d895976e3f757e1c762e
Mrichko Bohdan <zaq4769@gmail.com>
2024-08-23 02:41:45 +0300

    chore(types): add type declaration for global process variable

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    "import.meta.env.VITE_API_URL": JSON.stringify(process.env.VITE_API_URL),
+    "import.meta.env.VITE_API_URL": JSON.stringify(
+      process.env.VITE_API_URL || "http://0.0.0.0:8055"
+    ),
   },
   server: {
     port: 3000,
